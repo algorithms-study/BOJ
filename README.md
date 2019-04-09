2311ce5748873b09eb7b0b7e553f36728985128f
Mingeun Kim <mingeun.k.k@gmail.com>
2019-04-09 11:29:42 +0900

    Added Directory and Files Auto Make Script

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# C++ Algorithms Repo
+# Algorithms
 
 It starts in May.
+
+```bash
+$ chmod 777 make.sh
+$ ./make.sh start_num end_num
+```
---
+++ make.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$#" -lt 2 ]; then
+  echo "parameters is required"
+  exit 1
+fi
+
+files=("js" "py" "cpp")
+
+for ((i = $1; i <= $2; i++))
+do
+  mkdir $i
+  cd $i
+  for file in ${files[*]}
+  do
+    mkdir $file
+    touch $file/$i.$file
+  done
+  cd ..
+done
