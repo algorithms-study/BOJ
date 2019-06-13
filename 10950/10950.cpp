fd13d22012d259a804cd29066ba71d91edab0bec
Mingeun Kim <mingeun.k.k@gmail.com>
2019-06-13 15:31:04 +0900

    사칙연산: A + B - 3

---
+++ 10950/10950.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int T;
+
+int main() {
+  cin >> T;
+
+  for (int i = 0; i < T; i++) {
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b << endl;
+  }
+}
