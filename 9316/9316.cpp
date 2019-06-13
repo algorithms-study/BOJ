34213011eb1b00e5863bdcac16e995993a76c494
Mingeun Kim <mingeun.k.k@gmail.com>
2019-06-13 15:17:46 +0900

    Hello Judge

---
+++ 9316/9316.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main() {
+  cin >> N;
+  
+  for (int i = 1; i <= N; i++) {
+    cout << "Hello World, Judge " << i << "!" << endl;
+  }
+}
