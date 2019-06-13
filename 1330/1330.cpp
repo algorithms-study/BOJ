039d7ba697f05d38a00852749f77ccc5293f8e49
Mingeun Kim <mingeun.k.k@gmail.com>
2019-06-13 15:58:41 +0900

    두 수 비교하기

---
+++ 1330/1330.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int A, B;
+
+int main() {
+  cin >> A >> B;
+  
+  if (A > B) {
+    cout << ">";
+  } else if (A < B) {
+    cout << "<";
+  } else {
+    cout << "==";
+  }
+}
