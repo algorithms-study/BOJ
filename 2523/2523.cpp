26b6f2a9b29ec2931a8d9569768a8c4efa618608
Mingeun Kim <mingeun.k.k@gmail.com>
2019-06-16 16:03:56 +0900

    별 찍기 - 13

---
+++ 2523/2523.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main() {
+  cin >> N;
+
+  for (int i = 1; i < N; i++) {
+    for (int j = 1; j < i + 1; j++) {
+      cout << "*";
+    }
+
+    cout << '\n';
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = i; j < N; j++) {
+      cout << "*";
+    }
+
+    cout << '\n';
+  }
+}
