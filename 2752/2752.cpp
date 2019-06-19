daa890f25ae803c7e9c80617e5ce41523da0d762
Mingeun Kim <mingeun.k.k@gmail.com>
2019-06-19 14:44:36 +0900

    세수정렬

---
+++ 2752/2752.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> v;
+
+int main() {
+  for (int i = 0; i < 3; i++) {
+    int n;
+    cin >> n;
+
+    v.push_back(n);
+  }
+
+  sort(v.begin(), v.end());
+
+  for (int i = 0; i < v.size(); i++) {
+    cout << v[i] << " ";
+  }
+}
