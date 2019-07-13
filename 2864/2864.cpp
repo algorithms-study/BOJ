e10e44b0f6f6751bfab97e914fc1ef7cf88fc8ad
Mingeun Kim <mingeun.k.k@gmail.com>
2019-07-13 15:35:49 +0900

    문자열 처리: 5와 6의 차이

---
+++ 2864/2864.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+string A, B;
+int maximum, minimum;
+
+int main() {
+  cin >> A >> B;
+
+  for (int i = 0; i < A.length(); i++) {
+    if (A[i] == '5') {
+      A[i] = '6';
+    }
+  }
+
+  for (int i = 0; i < B.length(); i++) {
+    if (B[i] == '5') {
+      B[i] = '6';
+    }
+  }
+
+  maximum = stoi(A) + stoi(B);
+
+  for (int i = 0; i < A.length(); i++) {
+    if (A[i] == '6') {
+      A[i] = '5';
+    }
+  }
+
+  for (int i = 0; i < B.length(); i++) {
+    if (B[i] == '6') {
+      B[i] = '5';
+    }
+  }
+
+  minimum = stoi(A) + stoi(B);
+
+  cout << minimum << " " << maximum;
+}
